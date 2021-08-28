f4a6c71c24a463ef00fb49fa4c275fff1f2f4f38
Sthope <6695214+Sthopeless@users.noreply.github.com>
2021-08-28 22:37:07 +0200

    Create randomport1.py

---
+++ randomport/randomport1.py
@@ -0,0 +1,16 @@
+from random import *
+
+def randomPort():
+    x = randint(10000, 65535)
+    print(x)
+
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
+randomPort()
